a37c7f42ee51c93c92de83901d720a2fdb7f752f
RomanovaNI <romanovanataliade@gmail.com>
2021-02-17 13:57:35 +0300

    Sneginka

---
+++ 2021_02_17_Snowflock/src/Snowflake_Solution.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Snowflake_Solution {
+    public static void main(String[] args) {
+
+        //    winter(10);
+        String[][] snowflacke = winter(10);
+        myPrint(snowflacke);
+    }
+
+    private static void myPrint(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
+
+    private static String[][] winter(int size) {
+        String[][] output;
+        if (size % 2 == 0) {
+            output = new String[size + 1][size + 1];
+        } else {
+            output = new String[size][size];
+        }
+        //solution
+        fillArray(output, 'X');
+
+        return output;
+    }
+
+    private static void fillArray(String[][] input, char element) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                input[i][j]= String.valueOf(element);
+            }
+
+        }
+    }
+}
