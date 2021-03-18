2d6bacf51a7d2e5fa5de0b7b16718d04c099e1c4
RomanovaNI <romanovanataliade@gmail.com>
2021-03-18 09:25:35 +0100

    correct Comp by year and price

--- 2021_03_17_HW_06_Ivan_Compare/src/comparators/ComparatorByYearAndPrice.java
+++ 2021_03_17_HW_06_Ivan_Compare/src/comparators/ComparatorByYearAndPrice.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class ComparatorByYearAndPrice implements Comparator<Car> {
     @Override
     public int compare(Car o1, Car o2) {
-        int res = o1.getYear() - o2.getYear();
-        return res == 0 ? Double.compare(o1.getPrice(), o2.getPrice()) : res;
+        int res = Double.compare(o1.getPrice(),o2.getPrice());
+        return res == 0 ? o1.getYear() - o2.getYear() : res;//esli prtice odinakovyie, togda sravnis po year, a esli ne odinakovyie(price) prodolgi sravnivat po price
     }
 }
